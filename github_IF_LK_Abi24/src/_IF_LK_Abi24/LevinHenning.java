8006e9cef5ab423704bd7ef1071773c7fba5351f
henkle <henkle@334PC10.schule.local>
2023-09-12 08:22:18 +0200

    HenningLevin

---
+++ github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
@@ -0,0 +1,11 @@
+package _IF_LK_Abi24;
+
+public class LevinHenning {
+	public LevinHenning() {
+		System.out.println("LevinHenning");
+	}
+	
+	public static void main(String[] args) {
+		new LevinHenning();
+	}
+}
