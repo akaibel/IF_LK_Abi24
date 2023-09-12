80f9f3ed7790989844725c119e48f3413908afe1
levvah <levvah@334PC09.schule.local>
2023-09-12 08:35:58 +0200

    neue version 8:32

--- github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
+++ github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
@@ -3,7 +3,10 @@ package _IF_LK_Abi24;
 public class LevinHenning {
 	public LevinHenning() {
 		System.out.println("LevinHenning");
+		System.out.println("Bauer234");
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		new LevinHenning();
