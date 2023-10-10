5dc212008def45ce749ffaa380dd09326fab62f2
akaibel <a.kaibel@googlemail.com>
2023-10-10 12:58:58 +0200

    spelling of c3po

--- github_IF_LK_Abi24/src/_IF_LK_Abi24/LehrerLaempel.java
+++ github_IF_LK_Abi24/src/_IF_LK_Abi24/LehrerLaempel.java
@@ -12,7 +12,7 @@ public class LehrerLaempel {
 		lieblingsSchueler = new ListWithViewer<String>();
 		lieblingsSchueler.append("Max");
 		lieblingsSchueler.append("Moritz");
-		lieblingsSchueler.append("C3PO");
+		lieblingsSchueler.append("C3POO");
 		lieblingsSchueler.append("R2D2");
 		lieblingsSchueler.append("Jobs"); // ***
 		lieblingsSchueler.append("Marvin the Robot");
