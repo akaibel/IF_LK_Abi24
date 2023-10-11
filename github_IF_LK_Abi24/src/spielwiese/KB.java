c19cb1dec7b54972abdd26d65ac9511dd61b888d
Andreas Kaibel <AndKai@sibi-honnef.de>
2023-10-11 12:55:21 +0200

    424242

--- github_IF_LK_Abi24/src/spielwiese/KB.java
+++ github_IF_LK_Abi24/src/spielwiese/KB.java
@@ -2,7 +2,6 @@ package spielwiese;
 
 public class KB {
 	public static void main(String[] args) {
-		System.out.println(3132222);
-		System.out.println(4242);
+		System.out.println(42424242);
 	}
 }
