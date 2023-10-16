dd0e458cd894edc6194bb1862af4687c2ccbf1ca
kkphil <philipkurth6@gmail.com>
2023-10-16 11:37:28 +0200

    casc

--- github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
+++ github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
@@ -3,9 +3,11 @@ package _IF_LK_Abi24;
 public class LevinHenning {
 	public LevinHenning() {
 
-		for(int i = 0; i < 10_000; i++){
-			System.out.println("Levin  & Henning = Doof");
+		for(int i = 1; i <= 10_000; i++){
+			System.out.println("Levin & Henning = " + i + "x Doof");
 		}
+
+		//änderung
 		
 	}
 	
