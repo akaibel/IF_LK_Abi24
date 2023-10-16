4ec55e3f7fa4f8c6bd163c3b3af869ac4c7c0981
Wiebke Ewald <WieEwa@sibi-honnef.de>
2023-10-16 11:34:13 +0200

    Hallo

--- github_IF_LK_Abi24/src/_IF_LK_Abi24/WiJe.java
+++ github_IF_LK_Abi24/src/_IF_LK_Abi24/WiJe.java
@@ -1,7 +1,9 @@
 package _IF_LK_Abi24;
 
 public class WiJe {
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		System.out.println("test"); //
+		int i = 2+6;
+		return i;
 	}
 }
