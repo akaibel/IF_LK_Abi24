2c48fc8d52bae7c0b064906c3a6485b910d21160
Vin <vin.kopprasch@gmail.com>
2023-10-16 11:36:41 +0200

    test

--- github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
+++ github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
@@ -2,10 +2,7 @@ package _IF_LK_Abi24;
 
 public class LevinHenning {
 	public LevinHenning() {
-
-		for(int i = 0; i < 10_000; i++){
-			System.out.println("Levin  & Henning = Doof");
-		}
+		System.out.println("Lösch dich");
 		
 	}
 	
