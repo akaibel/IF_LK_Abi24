4c95090d4ebfa5983e0e500ebaa58cb840c7af2a
Henning Klein <henning.klein@sibi-honnef.de>
2023-10-17 08:38:35 +0200

    Henning Test 12334

--- github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
+++ github_IF_LK_Abi24/src/_IF_LK_Abi24/LevinHenning.java
@@ -1,12 +1,15 @@
 package _IF_LK_Abi24;
 
 public class LevinHenning {
-		public LevinHenning() {
+t	public LevinHenning() {
 
-		for(int i = 1; i <= 10_000; i++){
-			System.out.println("Levin & Henning = " + i + "x schlau");
 		}
-		
-		
+//Henning 
+// DEMONSTRATION
+	}
+	
+	
+SDF 
+		new LevinHenning();
 	}
 }
