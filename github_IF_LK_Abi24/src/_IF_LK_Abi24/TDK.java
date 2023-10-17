65bd8d36e43cf6d9db426cf587b4cace9c812104
Tim Etscheid <TimEts@sibi-honnef.de>
2023-10-17 08:34:48 +0200

    .
    .

--- github_IF_LK_Abi24/src/_IF_LK_Abi24/TDK.java
+++ github_IF_LK_Abi24/src/_IF_LK_Abi24/TDK.java
@@ -4,4 +4,9 @@ public class TDK {
 	public static void main(String[] args) {
 		System.out.println("test");
 	}
+	public void hallo() {
+		for(int i=0;i<5;i++) {
+			System.out.println("Hallo Welt");
+		}
+	}
 }
